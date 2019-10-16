6c5df3bcce514ff1bdde0c1f7fdfa0dc98d7747d
Nabil Hesham <nabilhz1994@gmail.com>
2019-10-16 07:22:45 +0200

    test2

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -130,9 +130,13 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
